b35596b6b6c2207a8c126d8e9b5bfee03cbd89d7
MengyaCao <55506640+MengyaCao@users.noreply.github.com>
2021-06-06 02:07:11 -0700

    Update etl.py

--- etl.py
+++ etl.py
@@ -56,21 +56,18 @@ def process_log_file(cur, filepath):
     df = df[df['page'] == 'NextSong'].copy()
 
     # convert timestamp column to datetime
-    t = pd.to_datetime(df['ts'], unit = 'ms')
+    t = pd.to_datetime(df.ts, unit = 'ms')
     
     # insert time data records
-    time_data = (t, t.dt.hour, t.dt.day, t.dt.week, t.dt.month, t.dt.year, t.dt.weekday)
-    column_labels = ('start_time', 'hour', 'day', 'week', 'month', 'year', 'weekday')
-    time_df = pd.DataFrame.from_dict(dict(zip(column_labels, time_data)))
+    time_data = [t, t.dt.hour, t.dt.day, t.dt.week, t.dt.month, t.dt.year, t.dt.weekday]
+    column_labels = ['start_time', 'hour', 'day', 'week', 'month', 'year', 'weekday']
+    time_df = pd.DataFrame(dict(zip(column_labels, time_data)))
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
     user_df = df[["userId", "firstName", "lastName", "gender", "level"]]
-
-    # convert dataframe timestamp column to datetime
-    df["ts"] = pd.to_datetime(df["ts"], unit='ms')
     
     # insert user records
     for i, row in user_df.iterrows():
@@ -89,7 +86,7 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = [index+1, row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent]
+        songplay_data = [pd.to_datetime(row.ts, unit='ms'), row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent]
         cur.execute(songplay_table_insert, songplay_data)
 
 
